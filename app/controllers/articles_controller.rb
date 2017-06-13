20ebf281fa3939f53c5a593b2e10f7550113591b
Srijan Manandhar <srijan.manandhar@gmail.com>
2017-06-13 23:31:34 +0300

    added few gems

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new (article_params)
+        @article = Article.new!(article_params)
         @article.save
 
        flash.notice = "Article '#{@article.title}' Created!!"
